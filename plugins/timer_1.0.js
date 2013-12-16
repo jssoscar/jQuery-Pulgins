d23997603128041021c49582df665380b8e4a04f
jssoscar <785979885jss@163.com>
2013-12-16 14:49:43 +0800

    jQuery plugin for timer
    Signed-off-by: jssoscar <785979885jss@163.com>
    Version: 1.0
    Date: 2013-12-16 14:49:41

--- plugins/timer_1.0.js
+++ plugins/timer_1.0.js
@@ -3,15 +3,23 @@
  * Date         2013-12-15 15:00:40
  * Version      1.0
  * Description  jQuery plugin for timer
+ * Usage		$.timer({
+					startTime : new Date(2013,11,16,23,12,10),
+					endTime : new Date(2013,11,16,23,12,14),
+					serverTime : $("#serverTime").val(),
+					callback : function(){
+						alert("Hello world!");
+					}
+				});
  */
-jQuery.fn.timer = function(options) {
+jQuery.timer = function(options) {
     /**
      * The configuration for the timer plugin
      *  
      * @param {String} startTime : start time for the timer.The data format: 2013,11,15,23,59,59
      * @param {String} endTime : end time for the timer.The data format: 2013,11,15,23,59,59
      * @param {Function} callback : the timer code to be executed
-     * @param {Number} serverTime : the server time(mill seconds)
+     * @param {Number} serverTime : the server time(mill seconds).This parameter is required.
      * 
      * PS: Here,about the server time.Why not the client time?
      * The client time is not safe because client can define or change the time.
@@ -22,17 +30,21 @@ jQuery.fn.timer = function(options) {
      * is December 16th),the code may never be executed.
      * So,get the server time is the most effective and safest way to run the timer.
      * 
+     * Last,about the server time.This parmeter can be writed on the page.
+     * Such as,you can write the server timer in the page bottom of the page(because the bottom is stationary).
+     * eg: <input type="hidden" id="serverTime" value="116544788" name="serverTime">
      */
     var config = (function() {
         var defaultCfg = $.extend({
             startTime : null,
             endTime : null,
-            callback : null,
+            callback : function(){
+            },
             serverTime : null
         }, options);
         if(defaultCfg.startTime){
             try{
-                defaultCfg.startTime = new Date(defaultCfg.startTime).getMilliseconds();
+                defaultCfg.startTime = defaultCfg.startTime.getTime();
             }catch(error){
                 // TODO
                 defaultCfg.startTime = 0;
@@ -42,7 +54,7 @@ jQuery.fn.timer = function(options) {
         }
         if(defaultCfg.endTime){
             try{
-                defaultCfg.endTime = new Date(defaultCfg.endTime).getMilliseconds();
+                defaultCfg.endTime = defaultCfg.endTime.getTime();
             }catch(error){
                 // TODO
                 defaultCfg.endTime = 0;
@@ -51,8 +63,15 @@ jQuery.fn.timer = function(options) {
             defaultCfg.endTime = 0;
         }
         return defaultCfg;
-    });
+    })();
     
+    /**
+     * Compare the server time and the custom time
+     * 
+     * . Server between the start time and the end time: execute the callback code
+     * . Start time equal 0 : If the server time less than the end time,execute the callback code
+     * . End time equal 0 : If the server time greater than the start time,execute the callback code
+     */
     if(config.startTime !== 0 && config.endTime !== 0){
         if(config.startTime<= config.serverTime && config.serverTime <= config.endTime){
             config.callback();
