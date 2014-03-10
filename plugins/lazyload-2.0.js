cd77a297357b1571dc2d1f3aad3396728f156e6c
jssoscar <785979885jss@163.com>
2014-03-10 12:13:41 +0800

    Add the visible area height parameter
    Signed-off-by: jssoscar
    Version: 2.0
    Date: 2014-3-10 12:13:39

--- plugins/lazyload-2.0.js
+++ plugins/lazyload-2.0.js
@@ -2,9 +2,7 @@
  * Author		jssoscar
  * Date			2014-2-20 15:26:00
  * Description	jQuery plugin for the lazy load image
- * Attention	If you use this plugin,ensure your image element has property "data-src"
  * Usage		$(".lazyload").lazyload()
- * 
  * 
  * Version		1.0.0
  * Date			2014-2-20 15:26:00
@@ -20,15 +18,24 @@
  * 				.	Optimize the code
  * 				.	Cache the lazyload DOM element
  */
-$.fn.lazyload = function() {
-	var imgCollection = $(this), lazyloadLength = imgCollection.length, finished = true, loadImg = function() {
+
+/**
+ * Define the lazyload function
+ * 
+ * @param {Object} options : the customer configuration
+ * 			 @param {Number} height : the visible area height
+ */
+$.fn.lazyload = function(options) {
+	var defaultCfg = $.extend({
+		height : 800
+	},options),imgCollection = $(this), lazyloadLength = imgCollection.length, finished = true, loadImg = function() {
 		if (lazyloadLength === 0 || !finished) {
 			return;
 		}
 		finished = false;
 		var spliceIndex = [];
 		imgCollection.each(function(index) {
-			if ( typeof ($(this).attr("data-src")) != "undefined" && $(this).offset().top < $(document).scrollTop() + $(window).height() + 800) {
+			if ( typeof ($(this).attr("data-src")) != "undefined" && $(this).offset().top < $(document).scrollTop() + $(window).height() + defaultCfg.height) {
 				$(this).attr("src", $(this).attr("data-src")).removeAttr("data-src");
 				spliceIndex.push(index);
 			}
